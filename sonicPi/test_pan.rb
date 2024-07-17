8c93bbf2bc44795cdd1246834729c95338c8c616
Yu-T0000 <chiave.yu@gmail.com>
2024-07-17 18:07:26 +0900

    検知した物体の位置とパンを対応させた

--- sonicPi/test_pan.rb
+++ sonicPi/test_pan.rb
@@ -1,14 +1,21 @@
 set :x, 0
+set :y, 1
 live_loop :live do
   b = get[:x]
   print b
-  play chord(:F3, :M7), pan: b
+  play chord(:F3, :M7), pan: b, amp: get[:y]
+  if get[:y]>0.3
+    set :y, get[:y]-0.3
+  else
+    set :y, 0
+  end
   sleep 1
 end
 
 live_loop :receive do
-  a = sync "/osc*/pan/"
+  a,b = sync "/osc*/object/laptop"
   print a
   set :x, a
-  sleep 1
+  set :y, 1
+  sleep 0.25
 end
